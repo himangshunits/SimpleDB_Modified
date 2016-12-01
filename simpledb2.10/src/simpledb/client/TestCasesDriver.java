3e074fb6be0ca174d69d3112d3791321698163df
Himangshu Ranjan Borah <himangshu.nits@gmail.com>
2016-12-01 18:30:46 -0500

    Adding the New Filw for Test Buffer Flow.

---
+++ simpledb2.10/src/simpledb/client/TestCasesDriver.java
@@ -0,0 +1,14 @@
+package simpledb.client;
+import simpledb.server.SimpleDB;
+
+public class TestCasesDriver {
+
+	public static void main(String args[]) throws Exception {
+		// configure and initialize the database
+		SimpleDB.init(args[0]);
+		
+		TestNewBufferManagementFlow.testBufferReplacementPolicy();
+	}
+
+	
+}
--- simpledb2.10/src/simpledb/client/TestNewBufferManagementFlow.java
+++ simpledb2.10/src/simpledb/client/TestNewBufferManagementFlow.java
@@ -1,61 +1,11 @@
 package simpledb.client;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.util.ListIterator;
-
 import simpledb.buffer.Buffer;
 import simpledb.buffer.BufferMgr;
 import simpledb.file.Block;
-import simpledb.remote.RemoteDriver;
-import simpledb.remote.RemoteDriverImpl;
-import simpledb.server.SimpleDB;
-import simpledb.tx.recovery.LogRecord;
-import simpledb.tx.recovery.LogRecordIterator;
-import simpledb.tx.recovery.RecoveryMgr;
 
-public class TestNewRecoveryFlow {
-
-	public static void main(String args[]) throws Exception {
-		// configure and initialize the database
-		SimpleDB.init(args[0]);   
-		
-		testBufferReplacementPolicy();
-		/*
-		System.out.println("database server ready for Recovery Flow Testing");
-		//Gget the buffer manager
-		BufferMgr bufferMgr = SimpleDB.bufferMgr();
-		// Creating a Block.
-		Block blk1 = new Block("block_file", 1);
-		Buffer buff = bufferMgr.pin(blk1);
-		
-		//Create a RecoveryManager with id = 123.
-		int txid = 123;
-		RecoveryMgr rm = new RecoveryMgr(txid);
-		//Commit a transaction .
-		rm.commit();
-		//Recover a transaction.
-		rm.recover();
-		//Sample setInt
-		int lsn1 = rm.setInt(buff, 4, 1234);
-		int lsn2 = rm.setString(buff, 4, "Himangshu");
-		
-		buff.setInt(4, 1234, txid, lsn1);
-		//Flushing all transactions
-		bufferMgr.flushAll(txid);
-		//Using Log Record Iterator to print records .
-		LogRecordIterator it = new LogRecordIterator();
-		while(it.hasNext()){
-			System.out.println(it.next());
-		}
-		System.out.println("-------------------");
-		while(it.hasPrevious()){
-			System.out.println(it.previous()); 
-		}	   
-		*/   
-	}
-
-	private static void testBufferReplacementPolicy() {
+public class TestNewBufferManagementFlow {
+	public static void testBufferReplacementPolicy() {
 		// TODO Auto-generated method stub
 		BufferMgr bufferMgr = new BufferMgr(8);
 		
@@ -135,4 +85,5 @@ public class TestNewRecoveryFlow {
 			e.printStackTrace();
 		}
 	}
+
 }
